a98b94ab7496cd222819224a90c7f6edb49e38be
kennethmortonjr <kennethmortonjr@130.215.125.136>
2018-09-05 11:13:55 -0400

    Added enhanced error checking

--- src/HTTPClient.c
+++ src/HTTPClient.c
@@ -1,44 +1,44 @@
 #include "HTTPClient.h"
 
-int main(int argc, char const *argv[])
-{
-    // for testing
-    char *message = "Hello from Client";
-    long valRead;
-    char buffer[1024] = {0};
-    
-    // TODO eventually read this stuff in from command line args
-    char *serverURL = "www.google.com";
-    char *portNum = "80";
+int main(int argc, char const *argv[]) {
+	// for testing
+	char *message = "Hello from Client";
+	long valRead;
+	char buffer[1024] = { 0 };
 
-    struct addrinfo hints;
-    struct addrinfo *results;
-    int sockfd;
-    
-    // first, load up address structs with getaddrinfo():
-    memset(&hints, 0, sizeof hints);
-    hints.ai_family = AF_UNSPEC;
-    hints.ai_socktype = SOCK_STREAM;
-    
-    int status;
-    if ((status = getaddrinfo(serverURL, portNum, &hints, &results)) != 0) {
-        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(status));
-        return(EXIT_FAILURE);
-    }
-    
-    // make a socket
-    if((sockfd = socket(results->ai_family, results->ai_socktype, results->ai_protocol)) < 0) {
-        printf("\nError creating socket\n");
-        return(EXIT_FAILURE);
-    }
-    
-    // connect to the server
-    if((connect(sockfd, results->ai_addr, results->ai_addrlen)) < 0) {
-        printf("\nError connecting to server\n");
-        return(EXIT_FAILURE);
-    }
+	// TODO eventually read this stuff in from command line args
+	char *serverURL = "www.google.com";
+	char *portNum = "80";
 
-    send(sockfd, message, strlen(message), 0);
-    printf("Message sent to server\n");
-    return 0;
+	struct addrinfo hints;
+	struct addrinfo *results;
+	int sockFD;
+
+	// first, load up address structs with getaddrinfo():
+	memset(&hints, 0, sizeof(hints));
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
+
+	int status;
+	if ((status = getaddrinfo(serverURL, portNum, &hints, &results)) != 0) {
+		fprintf(stderr, "Error in getaddrinfo (client): %s\n",
+				gai_strerror(status));
+		return (EXIT_FAILURE);
+	}
+
+	// make a socket
+	if ((sockFD = socket(results->ai_family, results->ai_socktype, results->ai_protocol)) < 0) {
+		perror("Error in socket (client)");
+		return (EXIT_FAILURE);
+	}
+
+	// connect to the server
+	if ((connect(sockFD, results->ai_addr, results->ai_addrlen)) < 0) {
+		perror("Error in connect (client)");
+		return (EXIT_FAILURE);
+	}
+
+	send(sockFD, message, strlen(message), 0);
+	printf("Message sent to server\n");
+	return 0;
 }
--- src/HTTPServer.c
+++ src/HTTPServer.c
@@ -2,54 +2,57 @@
 
 int main(int argc, char const *argv[]) {
 
-	int backlog = 10; // how many pending connections queue will hold
+	const int backlog = 10; // how many pending connections the queue will hold
 
-	// read from command line
+	// TODO read from command line
 	char *portNum = "3490"; // the port users will be connecting to
 
-    struct sockaddr_storage clientAddress;
-    socklen_t clientAddressSize;
-    struct addrinfo hints;
+	struct sockaddr_storage clientAddress;
+	socklen_t clientAddressSize;
+
+	struct addrinfo hints;
 	struct addrinfo *results;
-    int sockFD;
+	int sockFD;
+
 	int connectionFD;
 
-    // first, load up address structs with getaddrinfo():
-    memset(&hints, 0, sizeof hints);
-    hints.ai_family = AF_UNSPEC;  // use IPv4 or IPv6, whichever
-    hints.ai_socktype = SOCK_STREAM;
-    hints.ai_flags = AI_PASSIVE;     // fill in my IP for me
+	// first, load up address structs with getaddrinfo():
+	memset(&hints, 0, sizeof(hints));
+	hints.ai_family = AF_UNSPEC; // use IPv4 or IPv6, whichever
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_flags = AI_PASSIVE; // fill in my IP for me
 
-    int status;
-    if((status = getaddrinfo(NULL, portNum, &hints, &results)) != 0) {
-        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(status));
-        return(EXIT_FAILURE);
-    }
+	int status;
+	if ((status = getaddrinfo(NULL, portNum, &hints, &results)) != 0) {
+		fprintf(stderr, "Error in getaddrinfo (server): %s\n",
+				gai_strerror(status));
+		return (EXIT_FAILURE);
+	}
 
-    // make a socket
-    if((sockFD = socket(results->ai_family, results->ai_socktype, results->ai_protocol)) < 0) {
-        printf("\nError creating socket\n");
-        return(EXIT_FAILURE);
-    }
+	// make a socket
+	if ((sockFD = socket(results->ai_family, results->ai_socktype, results->ai_protocol)) < 0) {
+		perror("Error in socket (server)");
+		return (EXIT_FAILURE);
+	}
 
-    // try to bind
-    if(bind(sockFD, results->ai_addr, results->ai_addrlen) < 0) {
-        //printf("%s\n", "Error binding");
-        perror("Error binding");
-        return(EXIT_FAILURE);
-    }
+	// try to bind
+	if (bind(sockFD, results->ai_addr, results->ai_addrlen) < 0) {
+		perror("Error in bind (server)");
+		return (EXIT_FAILURE);
+	}
 
-    printf("Bind correctly\n");
+	// liten
+	if (listen(sockFD, backlog) < 0) {
+		perror("Error in listen (server)");
+		return (EXIT_FAILURE);
+	}
 
-    listen(sockFD, backlog);
+	// now accept an incoming connection:
+	clientAddressSize = sizeof(clientAddress);
+	connectionFD = accept(sockFD, (struct sockaddr *) &clientAddress,
+			&clientAddressSize);
 
-    // now accept an incoming connection:
-
-    clientAddressSize = sizeof clientAddress;
-    connectionFD = accept(sockFD, (struct sockaddr *)&clientAddress, &clientAddressSize);
-
-    // ready to communicate on socket descriptor new_fd!
-
+	// ready to communicate on socket descriptor connectionFD
 
 	printf("end of server");
 	return 0;
