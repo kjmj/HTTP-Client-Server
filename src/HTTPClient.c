e6bfcc06ea035968702e073075b06389d8d87cea
kennethmortonjr <kennethmortonjr@kensmacbookpro.dyn.wpi.edu>
2018-09-06 21:58:40 -0400

    client now uses a non persistant request

--- src/HTTPClient.c
+++ src/HTTPClient.c
@@ -5,28 +5,27 @@ int main(int argc, char *argv[]) {
 	char *serverURL;
 	char *portNum;
 	bool computeRTT;
-	const int bufferSize = 128; // TODO buffer size?
+	const int bufferSize = 1; // TODO buffer size?
 	char buffer[bufferSize] = { 0 };
-
-	if(argc == 3) {
-		computeRTT = false;
-		serverURL = argv[1];
-		portNum = argv[2];
-	}
-	else if (argc == 4) {
-		computeRTT = true;
-		serverURL = argv[2];
-		portNum = argv[3];
-	}
-	else {
-		printf("USAGE: ./HTTPClient [-options] server_url port_number\n");
-		return (EXIT_FAILURE);
-	}
-	// localhost: "127.0.0.1";
 
 	struct addrinfo hints;
 	struct addrinfo *results;
 	int sockFD;
+
+	// read in command line args
+	if (argc == 3) {
+		computeRTT = false;
+		serverURL = argv[1];
+		portNum = argv[2];
+	} else if (argc == 4) {
+		computeRTT = true;
+		serverURL = argv[2];
+		portNum = argv[3];
+	} else {
+		printf("USAGE: ./HTTPClient [-options] server_url port_number\n");
+		return (EXIT_FAILURE);
+	}
+	// localhost: "127.0.0.1";
 
 	// first, load up address structs with getaddrinfo():
 	memset(&hints, 0, sizeof(hints));
@@ -58,34 +57,13 @@ int main(int argc, char *argv[]) {
 
 	// testing connection
 
-//	char *s1 = "Host: ";
-//
-//	printf("before: %s", s1);
-//
-//	char *s2 = "www.example.com";
-//
-//	size_t len1 = strlen(s1), len2 = strlen(s2);
-//	char *concat = (char*) malloc(len1 + len2 + 1);
-//
-//	memcpy(concat, s1, len1);
-//	memcpy(concat+len1, s2, len2+1);
-//	strcat(s1, s2);
-
-//	char *s3 = "\r\n";
-//	strcat(s1, s3);
-//	printf("after: %s", s1);
-
-	char *GETTEST = "GET / HTTP/1.1\r\n"
+	char *GET = "GET / HTTP/1.1\r\n"
 			"Host: www.example.com\r\n"
-			"Connection: keep-alive\r\n"
+			"Connection: close\r\n"
 			"\r\n";
-	//char *message = "Hello from Client";
 
-	//send(sockFD, message, strlen(message), 0);
-	send(sockFD, GETTEST, strlen(GETTEST), 0);
-
+	send(sockFD, GET, strlen(GET), 0);
 	printf("Request sent from client\n");
-
 
 	while (recv(sockFD, buffer, bufferSize, 0) > 0) {
 		fputs(buffer, stdout);
